b4d1ae5fc7616356d5f1b80fee3dcde139f47e0c
Icecow05 <oliver.nemeth04@gmail.com>
2023-11-10 18:03:53 +0100

    Palindrome Checker

--- palindrome_checker/main.py
+++ palindrome_checker/main.py
@@ -1,4 +1,17 @@
-with open('./input.txt', 'r') as f:
+with open('palindrome_checker/input.txt', 'r') as f:
   input = f.read()
+lines = input.splitlines()
 
-print(input)
+for line in lines: 
+  cleaned_line = ''.join(char for char in line if char.isalnum())
+  unique_chars = set()
+  for char in cleaned_line:
+    if char.isalnum():
+      unique_chars.add(char)
+    elif not char.isalnum():
+      char.remove(char)
+  logic = cleaned_line == cleaned_line[::-1]
+  if logic == True:
+    print(f"YES, {len(unique_chars)}")
+  else:
+    print("NO,", -1)
