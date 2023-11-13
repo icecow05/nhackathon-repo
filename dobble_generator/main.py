8117eebe1de1f454432fb07629cf4c6ac1a6fd7e
icecow05 <oliver.nemeth04@gmail.com>
2023-11-13 13:42:12 +0100

    Final version of dobble generator

--- dobble_generator/main.py
+++ dobble_generator/main.py
@@ -1,47 +1,48 @@
 from random import shuffle
 
+# Function to read input from a file
 def read_input(file_path):
     try:
         with open(file_path, 'r') as file:
             symbols = [line.strip() for line in file.readlines()]
-            return symbols
+            valid_symbols = []
+
+            # Iterate through each symbol read from the file
+            for symbol in symbols:
+                try:
+                    n = int(symbol)
+                    if n <= 0:
+                        print(symbol)
+                        print("invalid")
+                    else:
+                        valid_symbols.append(symbol)
+                # If the conversion to an integer fails, print the symbol and mark it as invalid
+                except ValueError:
+                    print(symbol)
+                    print("invalid")
+
+            return valid_symbols
+        
     except FileNotFoundError:
-        print("A fájl nem található.")
+        print("Input file not found.")
         return None
+    
     except Exception as e:
-        print(f"Hiba történt a beolvasás közben: {str(e)}")
+        print(f"There was an error during import: {str(e)}")
         return None
 
-def is_valid_input(symbols):
-    for symbol in symbols:
-        try:
-            n = int(symbol)
-            if n <= 0:
-                print("A szimbólumok számának pozitívnak kell lennie.")
-                return False
-        except ValueError:
-            print("A bemenet nem érvényes, csak pozitív egész számokat fogad el.")
-            return False
-    return True
-
+# File path for input symbols
 input_file_path = "./input.txt"
 symbol_numbers = read_input(input_file_path)
-#print(symbol_numbers)
-"""if symbol_numbers is not None and is_valid_input(symbol_numbers):
-    print("Az input érvényes.")
-else:
-    print("Az input érvénytelen.")"""
 
+
+# Function to generate a Dobble deck
 def generate_dobble_deck(numberOfSymbolsOnCard, shuffleSymbolsOnCard=False):
     cards = []
 
-
     n = numberOfSymbolsOnCard - 1
 
-
-    numberOfCards = n**2 + n + 1
-
-
+    # Populate the cards list
     for i in range(n+1): 
         cards.append([1])
         for j in range(n):
@@ -54,17 +55,18 @@ def generate_dobble_deck(numberOfSymbolsOnCard, shuffleSymbolsOnCard=False):
                 val = (n+1 + n*k + (i*k+j)%n)+1
                 cards[len(cards)-1].append(val)
 
+    # Shuffle symbols on cards if specified
     if shuffleSymbolsOnCard:
         for card in cards:
             shuffle(card)
 
+    # Print each card with its index
     for i, card in enumerate(cards, 1):
         line = f"{i} - {card}"
         print(line)
 
+# Iterate through symbol numbers and generate Dobble decks
 for symbol_number in symbol_numbers:
     print(symbol_number)
     generate_dobble_deck(int(symbol_number), shuffleSymbolsOnCard=False)
     print()
-
-#generate_dobble_deck(30, shuffleSymbolsOnCard=False)
